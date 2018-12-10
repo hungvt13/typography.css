faff7902ec92293d12fa38fcef130ad1e893ba19
Hung Tran <sis.viethung@gmail.com>
2018-12-09 23:18:15 -0800

    Removed VueJs, implemented vanilla JS

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!',
-        selected: '',
-        options: [],
-        leftSideClass: 'left-side',
-    },
+let select = document.getElementById('mySelect');
+let val = select.value;
+const optionList = select.options;
+const options = [
+  { text: 'general', value: 'general' },
+  { text: 'high-end', value: 'high-end' },
+  { text: 'modern', value: 'modern' },
+  { text: 'start-up', value: 'start-up' },
+  { text: 'elegant', value: 'elegant' },
+  { text: 'attention', value: 'attention' },
+  { text: 'medieval', value: 'medieval' },
+  { text: 'art', value: 'art' },
+];
+options.forEach(option =>
+  optionList.add(
+    new Option(option.text, option.value,option.selected)
+  ));
 
-    beforeMount(){
-      this.$data.selected = 'general';
-      this.$data.options = [
-        { text: 'general', value: 'general' },
-        { text: 'high-end', value: 'high-end'},
-        { text: 'modern', value: 'modern' },
-        { text: 'start-up', value: 'start-up' },
-        { text: 'elegant', value: 'elegant' },
-        { text: 'attention', value: 'attention'},
-        { text: 'medieval', value: 'medieval' },
-        { text: 'art', value: 'art' },
-      ];
-    }
-});
+//set option
+const setSelected = (ele,val) =>{
+  document.getElementById(ele).classList.add(val);
+};
+
+//get current selected style
+const getSelected = () => {
+  return document.getElementById('mySelect').value;
+};
+
+//set initial value match the select option
+setSelected('sample-text',options[0].value);
+const changeClass = () => {
+  let classList = document.getElementById('sample-text').classList;
+  const selectedStyle = getSelected();
+  classList.remove(classList[1]);
+  classList.add(selectedStyle);
+}
