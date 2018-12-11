7d135917a449dfb186207f75b40af4e720f9be6e
Hung Tran <sis.viethung@gmail.com>
2018-12-11 04:25:34 -0800

    beautify codes ctn

--- js/mainForm.js
+++ js/mainForm.js
@@ -1,19 +1,58 @@
 let select = document.getElementById('select-preset');
 let val = select.value;
 const optionList = select.options;
-const options = [
-  { text: 'general', value: 'general', valueH:'general_header', valueP:'general_paragraph' },
-  { text: 'high-end', value: 'high-end', valueH:'high-end_header', valueP:'high-end_paragraph' },
-  { text: 'modern', value: 'modern', valueH:'modern_header', valueP:'modern_paragraph' },
-  { text: 'start-up', value: 'start-up', valueH:'start-up_header', valueP:'start-up_paragraph' },
-  { text: 'elegant', value: 'elegant', valueH:'elegant_header', valueP:'elegant_paragraph' },
-  { text: 'attention', value: 'attention', valueH:'attention_header', valueP:'attention_paragraph' },
-  { text: 'medieval', value: 'medieval', valueH:'medieval_header', valueP:'medieval_paragraph' },
-  { text: 'art', value: 'art', valueH:'art_header', valueP:'art_paragraph' },
+const options = [{
+    text: 'general',
+    value: 'general',
+    valueH: 'general_header',
+    valueP: 'general_paragraph'
+  },
+  {
+    text: 'high-end',
+    value: 'high-end',
+    valueH: 'high-end_header',
+    valueP: 'high-end_paragraph'
+  },
+  {
+    text: 'modern',
+    value: 'modern',
+    valueH: 'modern_header',
+    valueP: 'modern_paragraph'
+  },
+  {
+    text: 'start-up',
+    value: 'start-up',
+    valueH: 'start-up_header',
+    valueP: 'start-up_paragraph'
+  },
+  {
+    text: 'elegant',
+    value: 'elegant',
+    valueH: 'elegant_header',
+    valueP: 'elegant_paragraph'
+  },
+  {
+    text: 'attention',
+    value: 'attention',
+    valueH: 'attention_header',
+    valueP: 'attention_paragraph'
+  },
+  {
+    text: 'medieval',
+    value: 'medieval',
+    valueH: 'medieval_header',
+    valueP: 'medieval_paragraph'
+  },
+  {
+    text: 'art',
+    value: 'art',
+    valueH: 'art_header',
+    valueP: 'art_paragraph'
+  },
 ];
 
 //apply option[class] to a class byid
-const setSelected = (ele,val) => document.getElementById(ele).classList.add(val);
+const setSelected = (ele, val) => document.getElementById(ele).classList.add(val);
 
 //get a class list from id
 const getClassList = (ele) => document.getElementById(ele).classList;
@@ -23,11 +62,11 @@ const getSelected = (ele) => document.getElementById(ele).value;
 
 options.forEach(option =>
   optionList.add(
-    new Option(option.text, option.value,option.selected)
+    new Option(option.text, option.value, option.selected)
   ));
 
 //set initial value match the select option
-setSelected('sample-text',options[0].value);
+setSelected('sample-text', options[0].value);
 let classList = getClassList('sample-text');
 
 const changeClass = () => {
