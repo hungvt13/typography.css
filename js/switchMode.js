db7c0fdb171e6359cdfcc83b3ed84abd7da14bb2
ako2013 <sis.viethung@gmail.com>
2019-01-22 02:59:34 -0700

    Adjust for big screens + refractor switchEvent

--- js/switchMode.js
+++ js/switchMode.js
@@ -1,5 +1,6 @@
 let nightMode = false;
 class switchEvent{
+  // When switch toggle is click
   static switchClicked() {
     const nightBg = '#141c29';
     const nightTxt = 'white';
@@ -7,31 +8,33 @@ class switchEvent{
     const dayBg = 'white';
     const dayTxt = '#3b4351';
     const dayTxtCust = '#007EA7';
-  
+
     if(!nightMode){
       switchEvent.switchMode(nightBg, nightTxt, nightTxtCust);
+      switchEvent.changeTextColor(nightBg);
       nightMode = true
     }
     else{
       switchEvent.switchMode(dayBg, dayTxt, dayTxtCust);
+      switchEvent.changeTextColor(dayTxt);
       nightMode = false
     }
   }
+  // Switch to different mode
   static switchMode(select, text, custText){
     document.body.style.backgroundColor = select;
     document.body.style.color = text;
-    document.getElementById('select-preset').style.color = select;
     document.getElementById('customizeBtn').style.color = custText;
     document.getElementById('content-panel').style.backgroundColor = select;
-    if(select !== 'white')
-    {
-      document.getElementById('select-header').style.color = select;
-      document.getElementById('select-paragraph').style.color = select;
-    }
+  }
+  // Change options text color
+  static changeTextColor(color){
+    document.getElementById('select-preset').style.color = color;
+    document.getElementById('select-header').style.color = color;
+    document.getElementById('select-paragraph').style.color = color;
   }
 }
 
 //Event: switch button - day/night mode
 document.getElementById('switch')
         .addEventListener('input', () => switchEvent.switchClicked());
-
